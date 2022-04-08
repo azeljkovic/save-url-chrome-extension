fb1f5c9c7d67814682198dc5372fdff0708a0397
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-04-08 21:02:20 +0200

    added saving urls to array

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-let saveButton = document.getElementById('input-btn');
-
+const saveButton = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 
 function logSomething() {
-  console.log('Button Clicked');
+  myLeads.push(inputEl.value);
+  // console.log(myLeads);
 }
 
 saveButton.addEventListener("click", logSomething);
+
+for(let i = 0; i < myLeads.length; i++){
+  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+}
